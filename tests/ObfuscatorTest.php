b63891e458c6359ad39dc5a413fdee00639e7e76
Pierre-Henry Soria <hi@ph7.me>
2018-01-22 22:27:37 +0000

    Add Unit tests

---
+++ tests/ObfuscatorTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @author           Pierre-Henry Soria <hi@ph7.me>
+ * @copyright        (c) 2018, Pierre-Henry Soria. All Rights Reserved.
+ * @license          CC-BY - http://creativecommons.org/licenses/by/3.0/
+ * @link             http://about.ph7.me
+ */
+
+class ObfuscatorTest extends PHPUnit_Framework_TestCase
+{
+    const EXPECTED_DATA_FILENAME = 'expected-data.txt';
+
+    /**
+     * @dataProvider codeSampleProvider
+     */
+    public function testToString($sData)
+    {
+        $oObfusationData = new Obfuscator($sData, 'Test!');
+
+        $this->assertEquals($this->getExpectedData(), $oObfusationData);
+    }
+
+    /**
+     * @return array
+     */
+    public function codeSampleProvider()
+    {
+        $sData = "echo 'This is PHP';";
+
+        return array(
+            array($sData)
+        );
+    }
+
+    /**
+     * @return bool|string
+     */
+    private function getExpectedData()
+    {
+        return file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . self::EXPECTED_DATA_FILENAME);
+    }
+}
